f6cd53a233f26a376b5eb3d1b8f7789d03295446
SiravitPhokeed <siravitsasa@gmail.com>
2021-11-03 22:36:55 +0700

    Set up Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./democrazy/templates/**/*.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+    plugins: [],
+}
